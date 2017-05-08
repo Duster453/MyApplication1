9ff3b8f73aee231a59bfb4ce3f419affdfce28cd
Kushal <kushalgupta0565@gmail.com>
2017-05-09 04:41:24 +0530

    cOMMIT 1

--- app/src/main/java/com/testangle/mobileprogramming/myapplication/MainActivity.java
+++ app/src/main/java/com/testangle/mobileprogramming/myapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.testangle.mobileprogramming.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.sql.SQLOutput;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("ANKIT IS MAHA CHUTIYA");
     }
 }
